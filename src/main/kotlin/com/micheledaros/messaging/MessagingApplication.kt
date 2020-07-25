9a059a057745d41b7116e135d2e06180d8e8372b
Michele Da Ros <michele.daros@signavio.com>
2020-07-25 12:16:12 +0200

    spring boot skeleton

---
+++ src/main/kotlin/com/micheledaros/messaging/MessagingApplication.kt
@@ -0,0 +1,11 @@
+package com.micheledaros.messaging
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessagingApplication
+
+fun main(args: Array<String>) {
+	runApplication<MessagingApplication>(*args)
+}

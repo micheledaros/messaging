62008a0c0b8c01dc7fe1fb0c98f427f44f6f4899
Michele Da Ros <michele.daros@signavio.com>
2020-07-26 23:24:35 +0200

    typo

--- src/test/kotlin/com/micheledaros/messaging/message/controller/MessageEndpointIT.kt
+++ src/test/kotlin/com/micheledaros/messaging/message/controller/MessageEndpointIT.kt
@@ -3,26 +3,16 @@ package com.micheledaros.messaging.message.controller
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.micheledaros.messaging.configuration.SpringProfiles.H2DB
 import com.micheledaros.messaging.configuration.SpringProfiles.LIQUIBASE_OFF
-import com.micheledaros.messaging.message.controller.MessageDtoMaker.DEFAULT_MESSAGE_DTO
-import com.micheledaros.messaging.message.controller.MessagesDtoMaker.DEFAULT_MESSAGES_DTO
 import com.micheledaros.messaging.message.controller.dto.PostMessageDto
 import com.micheledaros.messaging.message.domain.MessageMaker.DEFAULT_MESSAGE
 import com.micheledaros.messaging.message.domain.MessageService
-import com.micheledaros.messaging.user.controller.NickNameDto
 import com.micheledaros.messaging.user.controller.UserConverter
-import com.micheledaros.messaging.user.controller.UserEndpoint
-import com.micheledaros.messaging.user.controller.UserEndpointTestConfiguration
-import com.micheledaros.messaging.user.domain.UserMaker.DEFAULT_USER
 import com.natpryce.makeiteasy.MakeItEasy.a
 import com.natpryce.makeiteasy.MakeItEasy.make
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.verify
-import org.mockito.junit.jupiter.MockitoExtension
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
@@ -92,8 +82,6 @@ internal class MessageEndpointIT (
     @Test
     fun `getting from messages received specifying all the optional fields returns status code 200`() {
         val senderId = "receiver"
-        val limit = 2
-        val startingId = 3
 
         val result = mockMvc.perform(
                 MockMvcRequestBuilders
